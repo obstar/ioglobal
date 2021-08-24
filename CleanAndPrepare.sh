69053668d0c16c55504f7e8618a79881ea36f0cb
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-08-24 17:01:38 +0200

    updating formating for echo command

--- CleanAndPrepare.sh
+++ CleanAndPrepare.sh
@@ -1,15 +1,15 @@
-echo "Go inside git folder and refresh repository with new source files"
+echo "\t-> Go inside git folder and refresh repository with new source files"
 cd /Users/obstar/Repos/GitHub/mantis
 git clean -fxd
 git pull --prune
 git submodule update --recursive --init
 
-echo "Kill old mantis client processes on the agent machine"
+echo "\t-> Kill old mantis client processes on the agent machine"
 processPid=$(pgrep -f '^.*mantis.*$')
 if [ -z "$processPid" ]
 then
-        echo "process Pid is empty"
+        echo "\t-> mantis process Pid is empty"
 else
         kill $processPid
-        echo "mantis process PID $processPid has been killed"
+        echo "\t-> mantis process PID $processPid has been killed"
 fi
