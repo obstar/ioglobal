8402388334898742b3082bef834e37830e3dfe1d
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-08-24 17:23:45 +0200

    updating script to fix issue with sbt command 3

--- CompileAndRun.sh
+++ CompileAndRun.sh
@@ -1,5 +1,7 @@
 echo "  -> Compile"
-/Users/obstar/.sdkman/candidates/sbt/1.5.5/bin/sbt dist || { echo 'sbt dist command failed' ; exit 1; }
+echo "  -> PWD:"
+pwd
+/Users/obstar/.sdkman/candidates/sbt/1.5.5/bin/sbt dist
 
 echo "  -> Unzip compiled build and go inside folder"
 cd /Users/obstar/Repos/GitHub/mantis/target/universal/
