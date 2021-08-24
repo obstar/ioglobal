e628e96714a650732938e812846e3f9322d5531f
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-08-24 17:09:13 +0200

    allowing CompileAndRun.sh script to be run by buildkite

--- CompileAndRun.sh
+++ CompileAndRun.sh
@@ -1,11 +1,11 @@
-echo "Compile"
+echo "  -> Compile"
 sbt dist
 
-echo "Unzip compiled build and go inside folder"
+echo "  -> Unzip compiled build and go inside folder"
 cd /Users/obstar/Repos/GitHub/mantis/target/universal/
 unzip -a \*.zip
 name=$(find . -name "mantis-*SNAPSHOT" ! -path "*.zip")
 cd $name
 
-echo "Run mantis"
-./bin/mantis-launcher etc | &!
+echo "  -> Run mantis"
+./bin/mantis-launcher etc &!
