f04d3361c37e515d52a9f3767512fe8fb6affee4
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-08-24 16:13:41 +0200

    added new script CleanAndPrepare.sh

---
+++ CleanAndPrepare.sh
@@ -0,0 +1,14 @@
+echo "Go inside git folder and refresh repository with new source files"
+cd /Users/obstar/Repos/GitHub/mantis
+git clean -fxd
+git pull --prune
+git submodule update --recursive --init
+
+echo "Kill old mantis client processes on the agent machine"
+processPid=$(pgrep -f '^.*mantis.*$')
+if [ -z "$processPid" ]
+then
+      echo "process Pid is empty"
+else
+      kill $processPid
+fi
